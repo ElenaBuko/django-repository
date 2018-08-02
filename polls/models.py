de8ca16865ec7f8ae64e86258af4f7d5aa944311
elenaBuko <elena.buko@mail.ru>
2018-08-02 17:29:16 +0300

    added new model

--- polls/models.py
+++ polls/models.py
@@ -11,3 +11,19 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class SmallObjectsManager(models.Manager):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(width__lt=200)
+
+class Image(models.Model):
+    width = models.IntegerField(null=False)
+    height = models.IntegerField(null=False)
+
+    objects = models.Manager()
+    small_objects = SmallObjectsManager()
+
+    def __str__(self):
+        return "Image(width={}, height={})".format(self.width, self.height)
